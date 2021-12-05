cb1c91b937ba1fe3fab8ef4f5b3de8ea7530f568
Lukas Frei <lukas.frei@zuehlke.com>
2021-12-05 15:17:03 +0100

    Setup project structure and add dependencies

---
+++ src/main/java/com/redbeard/kafka/KafkaJoinerApplication.java
@@ -0,0 +1,13 @@
+package com.redbeard.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJoinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaJoinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redbeard/kafka/KafkaJoinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbeard.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJoinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
